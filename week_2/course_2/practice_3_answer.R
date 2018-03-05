9181921f2d7b27e553dfa6c3bb134b5f0f4df78f
kbjiyu <KBJIYU@users.noreply.github.com>
2018-03-05 19:47:24 +0800

    [Add practice_3] version 1

--- week_2/course_2/practice_3_answer.R
+++ week_2/course_2/practice_3_answer.R
@@ -1,13 +1,37 @@
-### practice_3_answer
-### Crawler_Basic_A with XML
+### practice_3
+### Crawler Example
+
+### Crawler_Example with rvest    #####################################################################
+
+# https://blog.gtwang.org/r/rvest-web-scraping-with-r/
+rm(list = ls())
+library(rvest)
+
+# Set url
+url <- "https://www.ptt.cc/bbs/NBA/index5720.html"
+# Get response
+res <- read_html(url)
+
+# Parse the content and extract the titles
+raw.titles <- res %>% html_nodes("div.title")
+
+# Extract link
+nba.article.link <- raw.titles %>% html_node("a") %>% html_attr('href')
+
+# Extract article
+nba.article.title <- raw.titles %>% html_text()
+
+# Create dataframe
+nba.df <- data.frame(nba.article.title, nba.article.link)
+
+# Set df's colnames
+nba.df.col.names <- c("title", "link")
+colnames(nba.df) <- nba.df.col.names
 
 
-### Crawler_Basic_B with rvest
-
-
-### Crawler_Basic_C with jsonlite
-
-
-
-
+### Crawler_Example with jsonlite #####################################################################\
+rm(list = ls())
+library(jsonlite)
+url <- "https://www.dcard.tw/_api/posts?popular=true"
+res <- fromJSON(url)
 
