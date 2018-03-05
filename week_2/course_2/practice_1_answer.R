87d6a1eca938a90385a5f16bd37994b775ff09fb
bigmoumou <o1r2g3a4n5i6z7e8@gmail.com>
2018-03-05 22:04:54 +0800

    add week2 practice_1_answer

--- week_2/course_2/practice_1_answer.R
+++ week_2/course_2/practice_1_answer.R
@@ -2,9 +2,60 @@
 ### function 
 
 ### function SayHello
+SayHello <- function (name) {
+  print(paste("Hello,", name))
+}
+
 
 ### function GetBmi
+GetBmi <- function (my.height.cm, my.weight.kg) {
+  # Craete variable called my.height.cm with your actual height in cm 
+  my.height.cm <- 176
+  
+  # Craete variable called my.weight.cm with your actual weight in kg
+  my.weight.kg <- 69
+  
+  # Create my.height.m by my.height.cm 
+  my.height.m <- my.height.cm / 100
+  
+  # Create my.bmi with BMI(Body Mass Index) formula
+  my.bmi <- my.weight.kg / (my.height.m) ^ 2  
+  
+  # Return the value to assigned variable
+  return(my.bmi)
+}
+
 
 ### function CheckBmiLevel
+CheckBmiLevel <- function (my.height.cm, my.weight.kg) {
+  # Call the GetBmi function we just made
+  my.bmi = GetBmi(my.height.cm, my.weight.kg)
+  
+  if (my.bmi >= 35) {
+    print(paste("Your bmi: ", my.bmi))
+    print("重度肥胖!")
+  } else if (my.bmi >= 30) {
+    print(paste("Your bmi: ", my.bmi))
+    print("中度肥胖!")
+  } else if (my.bmi >= 27) {
+    print(paste("Your bmi: ", my.bmi))
+    print("輕度肥胖!")
+  } else if (my.bmi >= 24) {
+    print(paste("Your bmi: ", my.bmi))
+    print("過重!")
+  } else if (my.bmi >= 18.5) {
+    print(paste("Your bmi: ", my.bmi))
+    print("正常範圍")
+  } else {
+    print(paste("Your bmi: ", my.bmi))
+    print("過輕!")
+  }  
+}
+
 
 ### function GetLargest
+GetLargest <- function (vector) {
+  print(paste("The Largest Number is :", max(vector)))
+}
+
+
