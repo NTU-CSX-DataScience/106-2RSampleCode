af08051b2381441561a7131eed9f331c452105b2
pecu <pecu610@gmail.com>
2018-04-02 15:49:55 +0800

    [add task_6]

---
+++ week_6/task_6/FBcrawler/fb.R
@@ -0,0 +1,56 @@
+library(httr)
+
+name = "partytsulin"
+token = "EAACEdEose0cBAGs7Bcj5mI7RXZBORz6C5CY66ZCr4P8EZCgI7pdunsmWqZCo6ZC8IZB61lZCfukreU9xZCIw0UuhZAY24BnUvUd62WitbyHNI0NHthIb1STInWwfEsIo0zyulHUqfZB2JAYZCf2aI3s9dDS9B8j9VdlxLHxsMawBCLH1EZBtpHJz1QfEJqkZCPCwOYYYZD"
+
+url = paste0("https://graph.facebook.com/v2.12/",
+             name,
+             "?fields=posts&access_token=",
+             token)
+res = httr::GET(url)
+post = content(res)
+
+while( !is.null(url) )
+{
+  pageNo = length(data)
+  if( is.null(post$posts$data) )
+  {
+    data = post$data  
+  }
+  else
+  {
+    data = post$posts$data
+  }
+  date = data[[1]]$created_time
+  currentY = strsplit(date, '-')[[1]][1]
+  currentM = strsplit(date, '-')[[1]][2]
+  currentM = paste0(currentY,'_',currentM)
+  from = 1
+  saveData = list(data[[from]])
+
+  for( id in c(2:pageNo) )
+  {
+    date = data[[id]]$created_time
+    year = strsplit(date, '-')[[1]][1]
+    month = strsplit(date, '-')[[1]][2]
+    month = paste0(year,'_',month)    
+    if( currentM == month )
+    {
+      to = id
+      saveData = append(saveData, data[[to]])
+    }
+    else
+    {
+      filename = paste0("./",name,"/",currentM,".txt")
+      currentM = month
+      write(unlist(saveData), filename, append = TRUE)
+      saveData = list()
+      from = to-1
+    }
+  }
+  
+  url = paste0(post$paging$`next`,
+               post$posts$paging$`next`)
+  res = httr::GET(url)
+  post = content(res)
+}
